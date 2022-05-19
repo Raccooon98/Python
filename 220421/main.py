feb2d03f081510f6a47d1e3c44b7805fcd27b848
Raccooon98 <junbs1498@likelion.org>
2022-05-19 15:33:32 +0900

    05.19수업내용

--- 220421/main.py
+++ 220421/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import table, ball
+import table, ball, bat
 
 x_velocity = 10
 y_velocity = 10
@@ -10,10 +10,23 @@ window.title("MyPong")
 my_table = table.Table(window, net_colour="green",vertical_net=True)
 
 my_ball = ball.Ball(table=my_table,x_speed=x_velocity,y_speed=y_velocity,width=24,height=24,colour="red",x_start=288,y_start=188)
+
+bat_L=bat.Bat(table=my_table, width=15,height=100,x_posn=20, y_posn=150,colour="blue")
+bat_R=bat.Bat(table=my_table, width=15,height=100,x_posn=575, y_posn=150,colour="yellow")
+
 def game_flow():
+    bat_L.detect_collision(my_ball)
+    bat_R.detect_collision(my_ball)
+
     my_ball.move_next()
     window.after(50, game_flow)
 
+window.bind("a",bat_L.move_up)
+window.bind("z",bat_L.move_down)
+window.bind("<Up>",bat_R.move_up)
+window.bind("<Down>",bat_R.move_down)
+
 game_flow()
+
 
 window.mainloop()
--- 220421/table.py
+++ 220421/table.py
@@ -21,6 +21,13 @@ class Table:
         y2 = oval.y_posn + oval.height
         c = oval.colour
         return self.canvas.create_oval(x1,y1,x2,y2,fill=c)
+    def draw_rectangle(self,rectangle):
+        x1=rectangle.x_posn
+        x2=rectangle.x_posn+rectangle.width
+        y1 = rectangle.y_posn
+        y2 = rectangle.y_posn + rectangle.height
+        c = rectangle.colour
+        return self.canvas.create_rectangle(x1,y1,x2,y2, fill=c)
 
     def move_item(self,item,x1,y1,x2,y2):
         self.canvas.coords(item,x1,y1,x2,y2)
